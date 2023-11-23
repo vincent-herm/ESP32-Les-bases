23b1881142826e9ed0f09f370feecaf2b653d9ec
Vincent <54708916+vincent-herm@users.noreply.github.com>
2023-11-23 14:59:41 +0100

    Update tempo-eclairage.py

--- tempo-eclairage.py
+++ tempo-eclairage.py
@@ -1,12 +1,12 @@
 from machine import Pin
 import time
 
-led = Pin(21, Pin.OUT)
-bp = Pin(39, Pin.IN)
+led = Pin(2, Pin.OUT)
+bp = Pin(25, Pin.IN)
 
 compt = 0
 while True:
-    if not bp.value():
+    if bp.value():
         compt = 20
         print("Light ON")
     print(compt)
@@ -17,4 +17,4 @@ while True:
     time.sleep(0.1)
     compt = compt - 1
     if compt == 0:
-        print("Light BBF")
+        print("Light OFF")
