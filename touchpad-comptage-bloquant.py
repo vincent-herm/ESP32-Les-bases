dcc176b439697b0157300aeafd2faa9f894577f5
Vincent <54708916+vincent-herm@users.noreply.github.com>
2023-11-24 13:42:47 +0100

    touchpad-comptage-bloquant.py

--- touchpad-comptage-bloquant.py
+++ touchpad-comptage-bloquant.py
@@ -4,7 +4,7 @@ from time import sleep
 tp1 = TouchPad(Pin(15))
 led_bleue = Pin(2, Pin.OUT)
 compt = 0
-seuil = 100
+seuil = 200
 ancien_etat = tp1.read()
 
 for boucle in range (10) :         # on fait juste 10 cycles, et on arrete
