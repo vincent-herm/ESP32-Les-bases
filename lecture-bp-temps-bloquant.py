1c1c25a04e2f02dcdbf2d94b6efae5b3e069eabd
Vincent <54708916+vincent-herm@users.noreply.github.com>
2023-11-24 15:23:11 +0100

    lecture-bp-temps-bloquant.py

--- lecture-bp-temps-bloquant.py
+++ lecture-bp-temps-bloquant.py
@@ -5,10 +5,10 @@ bp = Pin(25, Pin.IN)
 compt_temps = 0
 
 for t in range(5):           # on fait juste 5 cycles appui/relachement
-    while not bpB.value() :  # on attend l'appui sur le bp   
+    while not bp.value() :  # on attend l'appui sur le bp   
         sleep(.02)           # on attend 20 ms, 50 boucles/s suffisant
     compt_temps = 0   
-    while bpB.value() :      # on attend l'appui sur le bp   
+    while bp.value() :      # on attend l'appui sur le bp   
         compt_temps += 1     # on incremente toutes les 20 ms
         sleep(.02)           # delai entre deux boucles   
     print("Compteur : {0:2d}, soit : {1:2f} s".\
